c7b36c5115e650610a07d82ba92644ed421e98b8
ayman-tareq <tareq.ctec02@gmail.com>
2024-12-07 21:56:29 +0600

    test done!

--- main.py
+++ main.py
@@ -271,4 +271,4 @@ if run_analysis:
     else:
         st.info("No tweets available to display.")
 else:
-    st.info("Click 'test2!' to display the data.")
+    st.info("Click 'Run' to display the data.")
